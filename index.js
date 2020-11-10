d663d599aa580ea8df36d798cc1817757bfc3e03
Purnima  Lakshminarayanan <purnima.nyc@gmail.com>
2020-11-10 04:46:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 // str = "ban", program prints "b", "a", "n"
 const printString = (str) => {
   console.log(str[0])
-  while(i<str.length){
-    i++
-    printString(str[i])
+  if(str.length > 1) {
+    printString(str[i+1])
   }
+  // while(i<str.length){
+  //   i++
+  //   printString(str[i])
+  // }
 }
