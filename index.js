8c0c584d4528c6722269c2d1c31bddf14f00187a
Purnima  Lakshminarayanan <purnima.nyc@gmail.com>
2020-11-10 04:54:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here!
 // str = "ban", program prints "b", "a", "n"
-const printString = (str) => {
+function printString(str) {
   console.log(str[0])
   if(str.length > 1) {
     let newString = str.substring(1, str.length)
