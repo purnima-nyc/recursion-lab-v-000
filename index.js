453b0a7be03da2ebf4f454aeb53f6d9857acf5aa
Purnima  Lakshminarayanan <purnima.nyc@gmail.com>
2020-11-10 04:53:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const printString = (str) => {
   console.log(str[0])
   if(str.length > 1) {
     let newString = str.substring(1, str.length)
-    i++
-    printString(str[i])
+    printString(newString)
   }
 }
