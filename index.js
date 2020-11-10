8b671ba88f0bc334f85d1a91b57a929c1b9c8755
Purnima  Lakshminarayanan <purnima.nyc@gmail.com>
2020-11-10 04:44:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution here!
 // str = "ban", program prints "b", "a", "n"
 const printString = (str) => {
-  console.log(str[0])
   while(i<str.length){
     i++
     printString(str[i])
